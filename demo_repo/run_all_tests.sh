7794743ee957a34545eb63a5b11481bdc30e8340
bpetersob <bpetersob@gmail.com>
2021-03-10 09:56:36 -0800

    Adds coverage measurements when all tests are run.

--- demo_repo/run_all_tests.sh
+++ demo_repo/run_all_tests.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+set -e -x
+
 # Run all of the python tests.
 find . -name "*_test.py" -exec python -m pytest {} \;
+
+# Calculate coverage.
+coverage run --source=. -m pytest
+find . -name "*.py" -exec python -m pytest {} \;
+coverage html
+coverage report
