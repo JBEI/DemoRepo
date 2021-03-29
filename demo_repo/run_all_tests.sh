a493f70a9ea38b88d09dff062f6240f772a6f58c
bpetersob <bpetersob@gmail.com>
2021-03-29 08:24:15 -0700

    Simplifies run_all_tests.sh.

--- demo_repo/run_all_tests.sh
+++ demo_repo/run_all_tests.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -e -x
 
-# Run all of the python tests.
-find . -name "*_test.py" -exec python -m pytest {} \;
-
-# Calculate coverage.
+# Run all tests and calculate coverage.
 coverage run --source=. -m pytest
-find . -name "*.py" -exec python -m pytest {} \;
 coverage html
 coverage report
