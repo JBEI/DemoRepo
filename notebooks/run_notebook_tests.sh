7c39e97a410c45c3e928b9aea6dc68ba78af3918
Mark Forrer <mark.forrer@lbl.gov>
2023-01-23 19:11:25 -0800

    Update notebook tests
    Runipy is now deprecated since `jupyter nbconvert` includes a similar feature

--- notebooks/run_notebook_tests.sh
+++ notebooks/run_notebook_tests.sh
@@ -1,9 +1,7 @@
 #!/bin/zsh
 
-
-FILES=/path/to/*
 for f in *.ipynb
 do
   echo "Testing $f ..."
-  runipy "$f" 2>&1 | awk '/Cell raised uncaught exception/,EOF'
+  jupyter nbconvert --to notebook --execute "$f"
 done
